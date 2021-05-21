f058476803f667caa2914f7919978eed30077a9b
Developer-felix <onjombafelix123@gmail.com>
2021-05-21 06:09:22 -0700

    code modification and customisation

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+import time
 
-# Create your views here.
+doctors = {"Smith": "Anesthesiologist", 'Johnson': 'Cardiologist', "Williams":"Dermatologist", "Jones":"Neurologist"}
+timing =  ['9.00am', '9.30am', '10.00am','10.30am','11.00am','11.30am','12.00pm','12.30pm','1.00pm','3.00pm','3.30pm','4.00pm','4.30pm','5.00pm','5.30pm','6.00pm', '6.30pm', '7.00pm','7.30pm','8.00pm']
+
