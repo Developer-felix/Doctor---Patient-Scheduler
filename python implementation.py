a48e9584e649fec888192a3b90944f5bc078bd92
mrivera0111 <111253148+mrivera0111@users.noreply.github.com>
2022-12-01 20:45:40 -0500

    Update python implementation.py
    Updated timing to show as time increments.

--- python implementation.py
+++ python implementation.py
@@ -1,6 +1,6 @@
 import time
 doctors = {"Anson Smith, MD": "Anesthesiologist", 'Dominic Johnson, MD': 'Cardiologist', "Jennifer Williams, MD":"Dermatologist", "Melissa Jones, MD":"Neurologist", "Obi Ugwonali, MD":"Orthopedic Surgeon"}
-timing =  ['9.00am', '9.30am', '10.00am','10.30am','11.00am','11.30am','12.00pm','12.30pm','1.00pm','3.00pm','3.30pm','4.00pm','4.30pm','5.00pm']
+timing =  ['9:00am', '9:30am', '10:00am','10:30am','11:00am','11:30am','12:00pm','12:30pm','1:00pm','3:00pm','3:30pm','4:00pm','4:30pm','5:00pm']
 class Patient(object):
     def __int__(self,dc,sp,fn,ln,age,date):
         self.dc = dc
